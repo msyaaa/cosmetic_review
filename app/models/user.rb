069f4d9d330604f7a75133a5cbaa1756076e0879
milky <msy.9618@icloud.com>
2020-10-13 12:33:03 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    EMAILE_VALIDATION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, uniqueness: true, format: { with: EMAILE_VALIDATION }
+    PASSWORD_VALIDATION = /\A[a-zA-Z0-9]+\z/
+    validates :password, confirmation: true, format: { with: PASSWORD_VALIDATION }
+    validates :password_confirmation
+    validates :gender
+    validates :birthday
+  end
+  validates :gender, numericality: { other_than: 1} 
+  validates :introduce, length: { maximum: 1000 }
 end
