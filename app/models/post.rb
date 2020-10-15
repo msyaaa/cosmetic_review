c54e4ad4ec4283d67715d67df6c878e4c61eec4c
milky <msy.9618@icloud.com>
2020-10-15 17:22:57 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+
+  with_options presence: true do
+    validates :name, length: { maximum: 50 }
+    PRICE_VALIDATION = /\A[0-9]+\z/
+    validates :price, format: { with: PRICE_VALIDATION }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 10_000_000 }
+    validates :description, length: { maximum: 1000 }
+    validates :image, presence: { message: 'を選択してください' }
+    validates :evaluation, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0.5 }
+    validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
+  end
+  validates :shop_name, length: { maximum: 50 }
 end
