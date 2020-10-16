5eddd8d758f45592388d45be1afb965bc7dbe7ba
milky <msy.9618@icloud.com>
2020-10-16 14:56:29 +0900

    モデルの単体テストコード

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 10_000_000 }
     validates :description, length: { maximum: 1000 }
     validates :image, presence: { message: 'を選択してください' }
-    validates :evaluation, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0.5 }
+    validates :evaluation, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0.5 }, presence: { message: 'を選択してください' }
     validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
   end
   validates :shop_name, length: { maximum: 50 }
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :post do
-    
+      name { 'abc' }
+      price { 11_111 }
+      description { 'abcdefg' }
+      category_id { 2 }
+      evaluation { 2 }
+      association :user
   end
 end
