bc3a9c47441084cc951dfa0dea9a3932bff43c96
milky <msy.9618@icloud.com>
2020-10-23 14:25:27 +0900

    年齢表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    birthday = @user.birthday
+    date_format = "%Y%m%d"
+    @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
   end
 end
