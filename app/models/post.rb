96f88d946920d8a986cc14977d3268f7ecea61fe
milky <msy.9618@icloud.com>
2020-11-02 14:15:26 +0900

    アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_many :likes, dependent: :destroy
   has_one_attached :image
   # extend ActiveHash::Associations::ActiveRecordExtensions
