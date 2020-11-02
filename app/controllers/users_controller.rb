74bb57ecfac6e381bdf3fb9b62461b748ac3dfc5
milky <msy.9618@icloud.com>
2020-11-02 10:56:42 +0900

    プロフィール編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
   def show
-    @user = User.find(params[:id])
     birthday = @user.birthday
     date_format = "%Y%m%d"
     @age = (Date.today.strftime(date_format).to_i - birthday.strftime(date_format).to_i) / 10000
@@ -8,5 +8,24 @@ class UsersController < ApplicationController
 
     likes = Like.where(user_id: current_user.id).order(created_at: :desc).pluck(:post_id)
     @likes = Post.find(likes)
+  end
+
+  def edit
+  end
+  def update
+    if current_user.update(user_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :birthday, :introduce, :gender, :image, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     end
     resources :likes, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
