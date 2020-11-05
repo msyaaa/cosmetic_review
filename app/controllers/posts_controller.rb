9ff52e105a8b9d8f67b0681ea5873941f6cd3c49
milky <msy.9618@icloud.com>
2020-11-05 12:05:01 +0900

    カテゴリー検索ウィンドウ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,32 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :index_post, only: [:top, :index, :show]
+  before_action :set_parents
   def top
-    @categories = Category.all
+    # @categories = Category.all
+    # @children = Category.find(params[:parent_id]).children
+    respond_to do |format|
+      format.html
+      format.json do
+        if params[:parent_id]
+          @childrens = Category.find(params[:parent_id]).children
+        elsif params[:children_id]
+          @grandChilds = Category.find(params[:children_id]).children
+        elsif params[:gcchildren_id]
+          @parents = Category.where(id: params[:gcchildren_id])
+        end
+      end
+    end
   end
   def index
   end
   def new
     @post = Post.new
+    # @children = Category.find(params[:parent_id]).children
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
   end
 
   def create
@@ -55,5 +74,9 @@ class PostsController < ApplicationController
 
   def index_post
     @posts = Post.order(created_at: :desc)
+  end
+
+  def set_parents
+    @parents = Category.where(ancestry: nil)
   end
 end
