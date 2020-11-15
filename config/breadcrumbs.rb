574e8f9912daa326abbe7d9fe0c7ceed8b08df74
milky <msy.9618@icloud.com>
2020-11-15 11:20:00 +0900

    カテゴリー検索パンくず

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -16,6 +16,34 @@ crumb :user do |user|
   parent :root
 end
 
+# 親カテゴリーのパンくず
+crumb :parent_category do |category|
+  category = Category.find(params[:id]).root
+  link "#{category.name}", search_post_path(category)
+  parent :root
+end
+
+# 子カテゴリーのパンくず
+crumb :child_category do |category|
+  category = Category.find(params[:id])
+  # 表示しているページが子カテゴリーの一覧ページの場合
+  if category.has_children?
+    link "#{category.name}", search_post_path(category)
+    parent :parent_category
+
+  # 表示しているページが孫カテゴリーの一覧ページの場合
+  else
+    link "#{category.parent.name}", search_post_path(category.parent)
+    parent :parent_category
+  end
+end
+
+# 孫カテゴリーのパンくず
+crumb :grandchild_category do |category|
+  category = Category.find(params[:id])
+  link "#{category.name}", search_post_path(category)
+  parent :child_category
+end
 
 
 # crumb :projects do
